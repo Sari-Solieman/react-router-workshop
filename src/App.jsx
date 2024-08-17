5fd749c4abadad9413fd61fc7c6063def5a5034b
Sari <sari.solieman@gmail.com>
2024-08-17 15:59:02 +0300

    Install mui component library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Button from '@mui/material/Button';
 
 function App() {
 
     return (
         <>
             <h1>React workshop</h1>
+            <Button variant="contained">Hello world</Button>
         </>
     )
 }
